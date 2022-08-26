f66246aa5a3460540dce7455fc8a036e75530095
Lucas <lucastafarelbs@gmail.com>
2022-08-26 08:20:58 -0300

    removida a autorizacao por enquanto

--- fastify-hooks/on-request.js
+++ fastify-hooks/on-request.js
@@ -9,12 +9,9 @@ export default async function (request, reply) {
     if (!token) return reply.code(401).send()
     
     const isValid = jwt.verify(token, process.env.JWT_SECRET)
-    // console.log(request)
-    // console.log(request.context.config.url)
-    // console.log(request.context.config.method)
-    //
     if (!isValid || !Object.keys(isValid).length) return reply.code(401).send()
     
+    /* permissions
     const data = await this.dbConnection('role')
       .join('role_permission', 'role_permission.role_id', '=', 'role.id')
       .join('permission', 'permission.id', '=', 'role_permission.permission_id')
@@ -31,6 +28,7 @@ export default async function (request, reply) {
     if (!permissions.includes(trying)) { 
       return reply.code(403).send()
     }
+    */
 
   } catch (e) {
    reply.code(500).send(e.message)
